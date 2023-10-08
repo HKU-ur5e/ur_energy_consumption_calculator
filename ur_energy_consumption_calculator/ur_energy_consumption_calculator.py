db6e397d9db6526d29878bc81df6c200a960b54c
Luk Tze Ching <thomasluk624@gmail.com>
2023-10-09 01:42:06 +0800

    feat: add reset cumulative workdone, and using dict to store each joint name and its cumulative workdone
    Signed-off-by: Luk Tze Ching <thomasluk624@gmail.com>

--- ur_energy_consumption_calculator/ur_energy_consumption_calculator.py
+++ ur_energy_consumption_calculator/ur_energy_consumption_calculator.py
@@ -2,20 +2,25 @@ import rclpy
 from rclpy.node import Node
 
 from sensor_msgs.msg import JointState
-from std_msgs.msg import Float64
 from ur_custom_msgs.msg import JointWorkDoneStamped
+from std_srvs.srv import Trigger
 
 
 class EnergyConsumptionCalculator(Node):
 
     def __init__(self):
         super().__init__('energy_consumption_calculator')
+        # Subscription
         self.joint_states_subscription_ = self.create_subscription(
             JointState, '/joint_states', self.joint_states_callback, 10)
         self.joint_states_subscription_
+        # Publisher
         self.energy_consumption_publisher_ = self.create_publisher(
             JointWorkDoneStamped, '/estimated_energy_consumption', 10)
-        self.total_work_done = 0.0
+        # Service
+        self.reset_work_done_service = self.create_service(Trigger, '/reset_joint_work_done', self.reset_work_done_callback)
+        
+        self.joint_cumulative_work_done = {}
         self.previous_joint_position = None
 
     def joint_states_callback(self, msg: JointState):
@@ -29,16 +34,28 @@ class EnergyConsumptionCalculator(Node):
             joint_effort = abs(msg.effort[joint])
             joint_angular_displacement = abs(msg.position[joint] - self.previous_joint_position[joint])
             joint_work_done = joint_effort * joint_angular_displacement
-            
-            # Assign payload to JointWorkDoneStamped object
-            self.joint_work_done.name.append(msg.name[joint])
-            self.joint_work_done.work_done.append(joint_work_done)
-            self.total_work_done += joint_work_done
-            self.joint_work_done.total_work_done = self.total_work_done
-
+            if msg.name[joint] in self.joint_cumulative_work_done:
+                self.joint_cumulative_work_done[msg.name[joint]] += joint_work_done
+            else:
+                self.joint_cumulative_work_done[msg.name[joint]] = joint_work_done
             self.previous_joint_position[joint] = msg.position[joint]
 
+        # Assign payload to JointWorkDoneStamped object
+        self.joint_work_done.name = list(self.joint_cumulative_work_done.keys())
+        self.joint_work_done.work_done = list(self.joint_cumulative_work_done.values())
+        self.joint_work_done.total_work_done = sum(self.joint_cumulative_work_done.values())
+
         self.energy_consumption_publisher_.publish(self.joint_work_done)
+
+    def reset_work_done_callback(self, request, response: Trigger):
+        # Reset all joint work done values to zero
+        self.joint_cumulative_work_done = {}
+        self.previous_joint_position = None
+
+        # Return success response
+        response.success = True
+        response.message = 'Joint work done reset successful'
+        return response
 
 
 def main(args=None):
